5f504d71eaaf43b913379a59435106e7c1e1f3b4
majkolman <maj.kolman98@gmail.com>
2024-12-16 21:41:03 +0100

    Third person camera

---
+++ Assets/Scripts/ChangeCam.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCam : MonoBehaviour
+{
+    public GameObject thirdCam;
+    public Cinemachine.CinemachineFreeLook thirdCamScript;
+
+    //0-->First Person
+    //1-->Third Person
+    public int CamMode;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (CamMode == 1)
+            {
+                CamMode = 0;
+            }
+            else
+            {
+                CamMode += 1;
+            }
+            StartCoroutine(CamChange());
+        }
+    }
+
+    IEnumerator CamChange()
+    {
+        yield return new WaitForSeconds(0.01f);
+        if (CamMode == 0)
+        {
+            thirdCamScript.Priority = 1;
+            Invoke(nameof(DisableCam), 2f);
+            
+        }
+        if (CamMode == 1)
+        {
+            thirdCam.SetActive(true);
+            Invoke(nameof(SetPriority), 0.05f);
+        }
+    }
+
+    void DisableCam()
+    {
+        thirdCam.SetActive(false);
+    }
+
+    void SetPriority()
+    {
+        thirdCamScript.Priority = 11;
+    }
+
+}
