bf6206a3f0a9b8c710959f9ab1fa6e79f5d412a4
xPyke-stack <jakob.coper@gmail.com>
2025-01-11 19:25:40 +0100

    Main menu script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,35 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    GameObject pannelUI;
-
-    void Awake()
-    {
-        pannelUI = GameObject.Find("Background");
-    }
-
     public void PlayGame()
     {
-        SceneManager.LoadScene(0);
-        pannelUI.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetSceneByName("SampleScene").buildIndex);
     }
 
     public void PlayTutorial()
     {
-        SceneManager.LoadScene(1);
-        pannelUI.SetActive(false);
-    }
-
-    public void BackToMenu()
-    {
-        pannelUI.SetActive(true);
+        SceneManager.LoadScene(SceneManager.GetSceneByName("TutorialScene").buildIndex);
     }
 }
-
-
-/*
-what if new scene for mainmenu like everyone does it
-pause screen main menu button controlled in MainMenu? script (on click enable Background) how does it work with pausing
-play button scene change to SampleScene (buildindex 0), disables Background
-tutorial button scene change to TutorialScene (buildindex 1), disables Background
-*/
